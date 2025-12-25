a9dcd8f8b8e51dbbbd4c449e7cfb473527a06bd2
Thanyanee Aimchit <thanyanee.a@ku.th>
2025-12-25 15:03:35 +0700

    edit test

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -9,8 +9,8 @@ describe('Calculator', () => {
         expect(add(-2, -3)).toBe(-5);
     });
 
-    test('subtracts 5 - 3 to equal 2', () => {
-        expect(subtract(5, 3)).toBe(2);
+    test('subtracts 5 - 2 to equal 2', () => {
+        expect(subtract(5, 2)).toBe(3);
     });
 
     test('multiplies 4 * 3 to equal 12', () => {
